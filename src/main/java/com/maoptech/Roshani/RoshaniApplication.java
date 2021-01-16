1b4873011bf83d38bbfe0acf443b136c29da69de
Mukhtar Ahmad <mukhtar_payman@yahoo.com>
2021-01-16 00:02:56 -0500

    Project Creation Commit

---
+++ src/main/java/com/maoptech/Roshani/RoshaniApplication.java
@@ -0,0 +1,13 @@
+package com.maoptech.Roshani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoshaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoshaniApplication.class, args);
+	}
+
+}
